3af552c550e0c0321867094ceef3d01087baba73
nirberger <46655769+nirberger@users.noreply.github.com>
2019-01-17 14:51:23 +0200

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise3 here
+## allow connect www from apache 
+sudo sed -i 's/deny from all/allow from all/' /etc/apache2/sites-available/default
+sudo service apache2 reload
